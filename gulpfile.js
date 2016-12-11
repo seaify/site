e7067d087b39e32beb2a79312e4eea1d7d558bc7
seaify <dilin.life@gmail.com>
2016-12-11 15:54:46 +0800

    gulp config works

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+
+var livereload = require('gulp-livereload');
+
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+  console.log('style convert')
+    gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('src/css/'))
+        .pipe(livereload());
 });
 
+gulp.task('html', function() {
+  gulp.src('src/files/*.html')
+    .pipe(livereload());
+});
+
+
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function() {
+  gulp.watch('src/sass/*.scss',['styles']);
+  gulp.watch('src/files/*.html', ['html']);
+  livereload.listen();
 });
