981e65daa0a5d8cc77f0c4cbc2517abd312eedb5
seaify <dilin.life@gmail.com>
2016-12-15 16:27:07 +0800

    update js

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -7,7 +7,10 @@ $(function() {
       method: 'POST',
       data: {email: $('#email').val(), apiKey: 'e4afe9c4807e960c18bb4811dcb7f65d-us14', status: 'unsubscribed'},
       dataType: 'json',
-      url: 'https://us14.api.mailchimp.com/2.0/lists/288e1bb46c/members',
+      url: 'https://us14.api.mailchimp.com/3.0/lists/288e1bb46c/members',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", "Basic " + btoa("bmcd:e4afe9c4807e960c18bb4811dcb7f65d-us14'"));
+      },
       success: function(res) {
         console.log(res);
       }
