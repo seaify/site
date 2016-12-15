8f96a63e8cb83c27a6fec8a79e6bea8f4c490566
seaify <dilin.life@gmail.com>
2016-12-15 16:22:18 +0800

    update js

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -5,7 +5,7 @@ $(function() {
     console.log('submit subscribe')
     $.ajax({
       method: 'POST',
-      data: {email: 'ee.com', apiKey: 'e4afe9c4807e960c18bb4811dcb7f65d-us14', status: 'unsubscribed'},
+      data: {email: $('#email').val(), apiKey: 'e4afe9c4807e960c18bb4811dcb7f65d-us14', status: 'unsubscribed'},
       dataType: 'json',
       url: 'https://us14.api.mailchimp.com/2.0/lists/288e1bb46c/members',
       success: function(res) {
